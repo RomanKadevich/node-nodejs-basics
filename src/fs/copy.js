bbb02ebdac88b33246b6a645634c408da9899677
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-01-29 12:35:43 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,6 +1,51 @@
+import { access, mkdir, readdir, copyFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
-const copy = async () => {
+const filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(filename);
+const sourceFolderPath = join(__dirname, "files");
+const targetFolderPath = join(__dirname, "files_copy");
 
-};
+try {
+  await access(sourceFolderPath);
 
-await copy();
+  try {
+    await access(targetFolderPath);
+
+    throw new Error("FS operation failed: Target folder already exists");
+  } catch (accessError) {
+
+    if (accessError.code === "ENOENT") {
+      try {
+        await mkdir(targetFolderPath);
+
+        const files = await readdir(sourceFolderPath);
+
+        for (const file of files) {
+
+          const sourceFile = join(sourceFolderPath, file);
+          const targetFile = join(targetFolderPath, file);
+
+          await copyFile(sourceFile, targetFile);
+        }
+
+        console.log("Folder copied successfully");
+
+      } catch (createError) {
+        
+        console.error("Error copying folder:", createError.message);
+      }
+    } else {
+
+      console.error("Error accessing target folder:", accessError.message);
+
+    }
+  }
+} catch (error) {
+
+  console.error(
+    "FS operation failed: error accessing source folder:",
+    error.message
+  );
+}
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,26 +1,27 @@
-import { writeFile, access } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import { writeFile, access } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const create = async () => {
   const filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(filename);
-  const targetPath = join(__dirname, "files", "fresh.txt");
+  const targetPath = join(__dirname, 'files', 'fresh.txt');
 
   try {
     await access(targetPath);
 
-    throw new Error("FS operation failed: File already exists");
+    throw new Error('FS operation failed: File already exists');
   } catch (err) {
-    if (err.code === "ENOENT") {
+   
+    if (err.code === 'ENOENT') {
       try {
-        await writeFile(targetPath, "I am fresh and young");
-        console.log("File created successfully:", targetPath);
+        await writeFile(targetPath, 'I am fresh and young');
+        console.log('File created successfully:', targetPath);
       } catch (createErr) {
-        console.error("Error writing file:", createErr.message);
+        console.error('Error writing file:', createErr.message);
       }
-    } else {
-      console.log(err);
+    }else{
+        console.log(err)
     }
   }
 };
