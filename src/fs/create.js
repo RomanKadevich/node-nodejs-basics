4a28491dd0571f20e4df0462582823a62065f258
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-01-29 11:56:07 +0300

    feat: add creat func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { writeFile, access } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
 const create = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(filename);
+  const targetPath = join(__dirname, "files", "fresh.txt");
+
+  try {
+    await access(targetPath);
+
+    throw new Error("FS operation failed: File already exists");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      try {
+        await writeFile(targetPath, "I am fresh and young");
+        console.log("File created successfully:", targetPath);
+      } catch (createErr) {
+        console.error("Error writing file:", createErr.message);
+      }
+    } else {
+      console.log(err);
+    }
+  }
 };
 
 await create();
