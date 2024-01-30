c034cc48054a8e756a475e0bdb2614b45fd1b126
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-01-30 10:20:12 +0300

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createReadStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { createHash } from "crypto";
+import { dirname, join } from "node:path";
 
+const calculateHash = async () => {
+  try {
+    const filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(filename);
+    const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+    const fileStream = createReadStream(filePath);
+    const hashAlgorithm = createHash("sha256");
+
+    for await (const chunk of fileStream) {
+      hashAlgorithm.update(chunk);
+    }
+    const hashResult = hashAlgorithm.digest("hex");
+    
+    console.log(hashResult);
+  } catch (error) {
+    throw error;
+  }
+};
 await calculateHash();
