16ad723cdb6d8ae77ded5eb203695a8b2d0ac116
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-01-30 10:49:18 +0300

    feat: add read sream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 const read = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(filename);
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+  const readStream = createReadStream(filePath);
+  const dataHandler = (chunk) => {
+    process.stdout.write(chunk);
+  }
+  const errorHandler = (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  }
+  readStream.on('data',dataHandler );
+  readStream.on('error',errorHandler) 
 };
 
 await read();
