9ceb1ab11997a45e5516b818f2a05063918045e2
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-01-30 12:10:00 +0300

    feat: add transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,32 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Transform } from "stream";
+
+class Reverse extends Transform {
+  constructor(options) {
+    super(options);
+  }
+  _transform(chunk, encoding, callback) {
+    const reversedChunk = chunk.toString().split("").reverse().join("");
+    this.push(reversedChunk);
+    process.stdout.write(reversedChunk);
+    callback();
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(filename);
+  const fileToReadPath = join(__dirname, "files", "fileToRead.txt");
+  const readStream = createReadStream(fileToReadPath);
+  const reverseTransform = new Reverse();
+
+  const errorHandler = (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  };
+
+  readStream.pipe(reverseTransform).on("error", errorHandler);
 };
 
 await transform();
