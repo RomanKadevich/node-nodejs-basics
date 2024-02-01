7553961ff5aa8a98d64f5071d81bb3ae02fe0461
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-02-01 10:03:11 +0300

    feat: add compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import zlib from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(filename);
+  const fileToReadPath = join(__dirname, "files", "fileToCompress.txt");
+  const compressFilePath = join(__dirname, "files", "archive.gz");
+  const readStream = createReadStream(fileToReadPath);
+  const writeStream = createWriteStream(compressFilePath);
+
+  const errorHandler = (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  };
+  readStream
+    .pipe(zlib.createGzip())
+    .pipe(writeStream)
+    .on("error", errorHandler);
 };
 
 await compress();
